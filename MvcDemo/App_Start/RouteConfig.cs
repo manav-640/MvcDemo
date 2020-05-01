3bc664d5b02b562795c45b2ebd27b77aaed7567c
manav-640 <manavya2003@gmail.com>
2020-05-01 21:06:36 +0530

    Add project files.

---
+++ MvcDemo/App_Start/RouteConfig.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace MvcDemo
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            routes.IgnoreRoute("Show/War");
+            routes.MapMvcAttributeRoutes();
+
+            routes.MapRoute(
+                name: "first",
+                url: "Show/War/{id}",
+                defaults: new { controller = "Test", action = "GetMyName", id= UrlParameter.Optional, name= UrlParameter.Optional}
+            );
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ MvcDemo/Controllers/TestController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcDemo.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ViewResult GetMyView()
+        {
+            return View("Index");
+        }
+
+        //public string GetMyName()
+        //{
+        //    return "Manav";
+        //}
+
+        public string GetMyName(string id, string name)
+        {
+            return "My Id is: " + id +" my name is: "+ name;
+        }
+    }
+}
